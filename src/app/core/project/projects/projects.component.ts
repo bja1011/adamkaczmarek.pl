26ec2cc3cb25c527fa0a397cc8ddf2cd895d0be5
adam <kaczmarek@oskarwegner.pl>
2019-06-07 18:06:08 +0200

    Projects list animations enter/leave test.

--- src/app/core/project/projects/projects.component.ts
+++ src/app/core/project/projects/projects.component.ts
@@ -2,11 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { ProjectsService } from '../services/projects.service';
 import { Project } from '../models/project.model';
 import { take } from 'rxjs/operators';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-project',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'],
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: '0' }),
+        animate('.2s ease-out', style({ opacity: '1' })),
+      ]),
+      transition(':leave', [
+        style({ opacity: '1' }),
+        animate('.2s ease-out', style({ opacity: '0' })),
+      ]),
+    ]),
+  ],
 })
 export class ProjectsComponent implements OnInit {
 
@@ -45,5 +58,9 @@ export class ProjectsComponent implements OnInit {
     } else {
       this.processedProjects = this.projects.filter(project => project.tags.find(tag => this.selectedTags.includes(tag)));
     }
+  }
+
+  trackProject(index, project: Project) {
+    return project.id;
   }
 }
