40c386442a9b22f6c32a3bd0b82f754b30d19b26
adam <kaczmarek@oskarwegner.pl>
2019-07-09 16:40:27 +0200

    Project description as html.

--- src/app/core/project/services/projects.mock.ts
+++ src/app/core/project/services/projects.mock.ts
@@ -5,8 +5,17 @@ export const MOCK_PROJECTS: Project[] = [
     id: 1,
     name: 'Gamification platform v4',
     type: projectType.commercial,
-    shortDescription: 'A gamification platform with computer game to support the achievement of business goals.',
-    description: ``,
+    shortDescription: 'A gamification platform with a computer game to support the achievement of business goals.',
+    description:
+      `Platform uses computer games to support the achievement of business objectives. <br>
+      The key element of the platform is to lure users into the game virtual world, 
+      where the progress depends entirely on the gamers and their business achievements.
+      The better the users fulfil their sales, efficiency, quality and training plans, the more virtual currency,
+      which enables them to develop in the game world, they obtain.
+      A motivational strategy constructed in such a way contributes to an increase between 10% and 20% in the sales, 
+      promotes regular monitoring of business reports and supports long-term commitment of the users of a gamification platform. <br>
+      See more at <a href="https://oskarwegner.pl/en/index.html#strategy" target="_blank">Oskar Wegner</a> website.
+`,
     techDescription: `Built with Angular v2 and PhaserJS`,
     images: ['assets/images/farm.jpg'],
     tags: [
